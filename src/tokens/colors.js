b69e59c0aa2f0a2ef019ba6c74e8da73de15759c
Dario-Felipe <dariofelipe91@gmail.com>
2021-08-19 15:42:13 -0300

    feat: add new dependencies and configs

---
+++ src/tokens/colors.js
@@ -0,0 +1,18 @@
+const white = '#FFFFFF';
+const dark = '#000000';
+const primary = '#020024';
+const secondary = '#000000';
+const third = '#00d4ff';
+const gradiant =
+  'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)';
+
+const colors = {
+  white,
+  dark,
+  primary,
+  secondary,
+  third,
+  gradiant,
+};
+
+export default colors;
