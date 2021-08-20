dcfb7efab17a34c33c9c8be647878785bee58cfd
Dario-Felipe <dariofelipe91@gmail.com>
2021-08-20 17:42:02 -0300

    feat(loginform): add new LoginForm component

---
+++ src/components/LoginForm/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Input, Button } from '@gympass/yoga';
+
+const LoginForm = ({
+  handleSingIn,
+  inputInfo,
+  handleInput,
+  setInputInfo,
+  errorMessage,
+}) => (
+  <form
+    onSubmit={handleSingIn}
+    style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}
+  >
+    <Input.Email
+      label="E-mail"
+      name="login"
+      value={inputInfo.login}
+      onChange={handleInput}
+      onClean={() => setInputInfo({ ...inputInfo, login: '' })}
+    />
+
+    <Input.Password
+      label="Password"
+      name="password"
+      value={inputInfo.password}
+      onChange={handleInput}
+    />
+
+    <div>
+      <Button type="submit">Sing In</Button>
+    </div>
+
+    {errorMessage && (
+      <div>
+        <span style={{ color: 'red' }}>{errorMessage}</span>
+      </div>
+    )}
+  </form>
+);
+
+export default LoginForm;
