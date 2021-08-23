e39de9161d42c125e1ab7605d61ce627893ef5af
Dario-Felipe <dariofelipe91@gmail.com>
2021-08-23 20:33:42 -0300

    style(formlogin): adjust align on content

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -10,7 +10,12 @@ const LoginForm = ({
 }) => (
   <form
     onSubmit={handleSingIn}
-    style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: '10px',
+    }}
   >
     <Input.Email
       label="E-mail"
